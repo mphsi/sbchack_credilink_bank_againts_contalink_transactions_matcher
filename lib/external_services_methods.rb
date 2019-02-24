a2fe814b4726fe7d61ed0096c19ef13c98586b86
Paul Hernández <mpaul.hernandez@gmail.com>
2019-02-23 19:48:35 -0600

    Usar headers indicados como params

--- lib/external_services_methods.rb
+++ lib/external_services_methods.rb
@@ -1,5 +1,5 @@
 module ExternalServicesMethods
-  def request_headers
+  def bank_request_headers
     {
       'x-api-key' => ENV['API_KEY'],
       'X-Client' => ENV['CLIENT'],
@@ -8,12 +8,12 @@ module ExternalServicesMethods
     }
   end
 
-  def call_bank_web_service(endpoint, query_params = nil)
+  def call_bank_web_service(endpoint, headers, query_params = nil)
     uri           = URI.parse(endpoint)
     http          = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl  = uri.scheme == 'https'
     uri.query     = query_params.nil? ? nil : URI.encode_www_form(query_params)
-    request       = Net::HTTP::Get.new(uri.request_uri, request_headers)
+    request       = Net::HTTP::Get.new(uri.request_uri, headers)
 
     http.request(request)
   end
